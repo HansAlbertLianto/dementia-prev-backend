5360e575f0dda777fcc5d880bf5cfdae5e296cbd
hansalbertlianto <lianto.albert28@gmail.com>
2018-10-16 02:36:57 +0800

    models, tested rest framework

---
+++ dprev_backend/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    username = models.CharField(max_length=50)
+    username.unique = True
+
+    email = models.CharField(max_length=200)
+
+class Game(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class GameResult(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    game_played = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    score = models.IntegerField()
+
+    datetime_reached = models.DateTimeField(auto_now_add=True)
+
+class PhotoNamePair(models.Model):
+    game_with_photo = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    photo_link = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=100)
