47bf6c2a9279b02469658836a103aa58176181f3
hansalbertlianto <lianto.albert28@gmail.com>
2018-10-16 10:13:40 +0800

    added serializers

--- dprev_backend/models.py
+++ dprev_backend/models.py
@@ -2,21 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+# Users of DementiaPrev
 class DPrevUser(models.Model):
     full_name = models.CharField(max_length=100)
     username = models.CharField(max_length=50, unique=True)
     email = models.CharField(max_length=200)
 
+# A game created by a user of DementiaPrev
 class Game(models.Model):
-    creator = models.ForeignKey(DPrevUser, on_delete=models.CASCADE)
+    creator = models.ForeignKey(DPrevUser, related_name='games_made', on_delete=models.CASCADE)
 
+# The end-result of one particular game
 class GameResult(models.Model):
-    player = models.ForeignKey(DPrevUser, on_delete=models.CASCADE)
-    game_played = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(DPrevUser, related_name='user_result_history', on_delete=models.CASCADE)
+    game_played = models.ForeignKey(Game, related_name='game_result_history', on_delete=models.CASCADE)
     score = models.IntegerField()
     datetime_reached = models.DateTimeField(auto_now_add=True)
 
+# One question in the game
 class PhotoNamePair(models.Model):
-    game_with_photo = models.ForeignKey(Game, on_delete=models.CASCADE)
+    game_with_photo = models.ForeignKey(Game, related_name='photo_name_pairs', on_delete=models.CASCADE)
     photo_link = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
--- dprev_backend/serializers.py
+++ dprev_backend/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from dprev_backend.models import DPrevUser, Game, GameResult, PhotoNamePair
 from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,3 +11,44 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+# Shows score results of a particular game
+class GameResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameResult
+        fields = ('player', 'score', 'datetime_reached')
+
+# Shows a link to a photo and a name
+class PhotoNamePairSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoNamePair
+        fields = ('photo_link', 'name')
+
+# Shows the game, its photo and name pairs, and its score history
+class GameSerializer(serializers.ModelSerializer):
+    photo_name_pairs = PhotoNamePairSerializer(many=True)
+    game_result_history = GameResultSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Game
+        fields = ('photo_name_pairs', 'game_history')
+    
+    # Create a game and its pairs
+    def create(self, validated_data):
+        photo_name_pairs = validated_data.pop('photo_name_pairs')
+        game = Game.objects.create(**validated_data)
+
+        for photo_name_pair in photo_name_pairs:
+            PhotoNamePair.objects.create(**photo_name_pair)
+        
+        return game
+
+# Shows the user, the games made by the user, and the scores the
+# user has attained in the games
+class DPrevUserSerializer(serializers.ModelSerializer):
+    user_result_history = GameResultSerializer(many=True, read_only=True)
+    games_made = GameSerializer(many=True)
+
+    class Meta:
+        model = DPrevUser
+        fields = ('full_name', 'username', 'email', 'games_made', 'game_result_history')
